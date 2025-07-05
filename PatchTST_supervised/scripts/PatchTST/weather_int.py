3481bd435dbcafca834f6aea9decaf2c1afb18c4
Michilaubi <65369516+Michilaubi@users.noreply.github.com>
2025-07-05 08:07:14 +0200

    Update weather_int.py

--- PatchTST_supervised/scripts/PatchTST/weather_int.py
+++ PatchTST_supervised/scripts/PatchTST/weather_int.py
@@ -4,7 +4,7 @@ import subprocess
 # ensure log directory exists
 os.makedirs("./logs/LongForecasting", exist_ok=True)
 
-seq_len = 432
+seq_len = 40
 model_name = "PatchTST_Attention"
 root_path_name = "./dataset/"
 data_path_name = "weather_int.csv"
@@ -13,13 +13,13 @@ data_name = "custom"
 random_seed = 2021
 
 # change horizon to 10 steps
-for pred_len in [72]:
+for pred_len in [10]:
     cmd = [
         "python", "-u", "run_longExp_attention.py",
         "--random_seed", str(random_seed),
         "--is_training", "1",
         "--do_predict",
-        "--label_len", "72",                
+        "--label_len", "10",                
         "--root_path", root_path_name,
         "--data_path", data_path_name,
         "--model_id", f"{model_id_name}_{seq_len}_{pred_len}",
@@ -36,18 +36,18 @@ for pred_len in [72]:
         "--e_layers", "3",
         "--n_heads", "7",
         "--d_model", "140",
-        "--d_ff", "280",
-        "--dropout", "0.2",
-        "--fc_dropout", "0.2",
+        "--d_ff", "560",
+        "--dropout", "0.1",
+        "--fc_dropout", "0.1",
         "--head_dropout", "0",
-        "--patch_len", "24",
-        "--stride", "12",
+        "--patch_len", "8",
+        "--stride", "4",
         "--des", "Exp",
         "--train_epochs", "100",
         "--patience", "10",
         "--itr", "1",
         "--batch_size", "128",
-        "--learning_rate", "0.003",
+        "--learning_rate", "0.001",
         "--num_workers", "4",
         "--output_attention",
     ]
